afde8c1f468fed7f1669612236aadc72c5fe7d17
Ado <adorikill@gmail.com>
2018-11-20 19:03:59 +0100

    Update planches.py

--- planches.py
+++ planches.py
@@ -7,13 +7,35 @@ Inputs:
     - brep
     - origin
     - direction
+    - minWasteLength : Longueur minimale acceptable pour créer une nouvelle chute
+    - smallFirst : défini si on trie en priorité de courtes chutes ou des longues chutes
     - DB
+    - reuse
+
     
 Outputs:
     - breps
     - DB
 """
 
+
 # TODO: Algo de remplissage
 
 # TODO: Maj de la DB
+
+# WIP
+
+import rhinoscriptsyntax as rs
+
+intersection = rs.IntersectBreps(brep, origin)
+vertices = rs.PolylineVertices(intersection)
+
+x = [i[0] for i in vertices]
+y = [i[1] for i in vertices]
+
+corner1 = [min(x), min(y), 0]
+corner2 = [max(x), min(y), 0]
+corner3 = [max(x), max(y), 0]
+corner4 = [min(x), max(y), 0]
+
+breps = rs.AddPolyline([corner1, corner2, corner3, corner4, corner1])
