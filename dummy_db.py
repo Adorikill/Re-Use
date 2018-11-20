50a874e7b9e5f2d9c77b37dfcbf6d7f7a26098b3
Ado <adorikill@gmail.com>
2018-11-20 16:07:35 +0100

    dummy_db
    Database de test qui sert à rien du tout

---
+++ dummy_db.py
@@ -0,0 +1,12 @@
+# dummy_db : crée une database de test
+# 
+# Format d'une planche : [longueur, largeur, epaisseur, ID, dispo, reserve, achete]
+
+a = [
+    [15.4, 17.5, 10, 0, True, False, False],
+    [11.2, 23.1, 10, 1, True, False, False],
+    [50.3, 38.3, 10, 2, True, False, False],
+    [80.3, 29.1, 37, 3, True, False, False],
+    [56.7, 38.9, 37, 4, True, False, False]
+]
+a = a[:,3]
