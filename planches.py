f4e62bae5f57426eb89d41561d283dc04f154798
Ado <adorikill@gmail.com>
2018-11-23 17:12:33 +0100

    Update planches.py

--- planches.py
+++ planches.py
@@ -35,9 +35,9 @@ else:
 
 # WIP
 
-import rhinoscriptsyntax as rs
+import rhinoscriptsyntax as rs # Permet d'importer des fonctions depuis rhino; pour Dynamo, trouver le SDK
 
-intersection = rs.IntersectBreps(brep, origin)
+intersection = rs.IntersectBreps(brep, origin) # Avec 'brep' = la forme à créer et 'origin' = la database de planches 
 vertices = rs.PolylineVertices(intersection)
 
 x = [i[0] for i in vertices]
@@ -46,6 +46,25 @@ y = [i[1] for i in vertices]
 corner1 = [min(x), min(y), 0]
 corner2 = [max(x), min(y), 0]
 corner3 = [max(x), max(y), 0]
-corner4 = [min(x), max(y), 0]
+corner4 = [min(x), max(y), 0] # Pb avec le positionement de la planche; si on la place à partir de l'origine (point) du brep à observer, on rate une intersection 
+                              # (x max) dans le cas ou on aurait à observer un quadrilatère sans angle droit ni parallèles (ou juste si l'un des cotés du quadrilatère 
+                              # n'est pas parallèle au vecteur x de l'origine)
+                              # RESOLUTION : TOURNER LA FORME ? 
 
+# Verifier si le vecteur résultant de 'corner3 = [max(x), max(y), 0]' et corner2 = [max(x), min(y), 0]' a deux intersections (avec le brep à observer);
+# Si = true, alors on passe à l'étape d'après
+# Si = false, alors on doit prolonger le vecteur jusqu'à une intersection avec le brep (en corner2 [max(x), min(y), 0]) et créer une nouvelle surface à remplir
+
+# Comparer la surface obtenue qui fait [corners1, corner2, corner3, corner 4] avec la database pour vérifier si y'a un recoupement avec le max(y) et la planche qu'on veut utiliser
+
+# Check si on peut la couper et créer une chute selon les parmaètres en minWasteLength;
+# Si = true, alors on la coupe et stocke provisoirement la chute créée 'C'
+# Si = false, on essaye avec la prochaine planche de la liste jusqu'à ce que ce soit =true
+
+# On répète l'opération pour remplir le brep
+
+           # PB: que se passe-t-il quand on arrive au bout du brep et qu'il reste un angle contenu dans une poutre ? 
+           # 
+           
 breps = rs.AddPolyline([corner1, corner2, corner3, corner4, corner1])
+
