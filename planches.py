d0fe7cb1e6917297940076ceef1377c50ca20847
Ado <adorikill@gmail.com>
2018-11-26 17:23:01 +0100

    Update planches.py

--- planches.py
+++ planches.py
@@ -33,11 +33,39 @@ else:
 # récupérer la première planche :  p = planchesDispo[0]
 # largeur d'une planche p : p[1]
 
+
 # WIP
 
-import rhinoscriptsyntax as rs # Permet d'importer des fonctions depuis rhino; pour Dynamo, trouver le SDK
+# On créé une BoundingBox autour du Brep en input; ca permet de definir une origine absolue pour n'importe quelle forme et de construire les planches imaginaires à recouper avec la DB
 
-intersection = rs.IntersectBreps(brep, origin) # Avec 'brep' = la forme à créer et 'origin' = la database de planches 
+for brep
+
+brep = brep #On récupère l'input brep
+if brep:
+    box = rs.boundingBox(brep)
+        if box:
+        for point[0] in enumerate(box)
+        breps = rs.AddBox(point[]) #(C'est pas bon mais l'idée c'est de pouvoir avoir la bounding box construite pour la voir sur l'output 'breps')
+
+#TROUVER LE CODE POUR : 
+
+# PREMIERE PARTIE :
+#STOCKER QQ PART L'AIRE DE LA BOUNDING BOX
+#TESTER UN ROTATE DU BREP POUR VOIR SI ON PEUT AVOIR UNE PLUS PETITE BOUNDING BOX
+#SI OUI, ON RESTOCKE QQ PART LA NOUVELLE DONNEE, ET ON LA COMPARE POUR UN ENSEMBLE DE ROTATE SUR 360° (loop sur l'ensemble des 360° pour trouver la meilleure orientation)
+#ON CONSERVE LA PLUS PETITE BOUNDING BOX =>>> ALGO DE TRI OU ON GARDE LA PLUS PETITE DATA ? 
+
+# DEUXIEME PARTIE : 
+#ON UTILISE LA LARGEUR DE LA PREMIERE PLANCHE TRIEE DANS L'ALGO POUR EN CREER UNE ARRTIFICIELLE SUR ymax (boundingBox); ON TESTE LES INTERSECTIONS AVEC LE BREP D'ORIGINE
+#ON COMPARE LES DIMENSIONS DU RECTANGLE OBTENU AVEC LES INTERSECTIONS AVEC LA PREMIERE PLANCHE DE LA DB; SI ELLE EST ASSEZ LONGUE, ON LA GARDE, SINON ON ESSAYE AVEC UNE AUTRE PLANCHE
+#  
+
+
+for surface 
+
+import rhinoscriptsyntax as rs # Permet d'importer des fonctions depuis rhino
+
+intersection = rs.IntersectBreps(brep, origin) # Avec ['brep' = la forme à créer] et ['origin' = la database de planches] 
 vertices = rs.PolylineVertices(intersection)
 
 x = [i[0] for i in vertices]
@@ -46,25 +74,7 @@ y = [i[1] for i in vertices]
 corner1 = [min(x), min(y), 0]
 corner2 = [max(x), min(y), 0]
 corner3 = [max(x), max(y), 0]
-corner4 = [min(x), max(y), 0] # Pb avec le positionement de la planche; si on la place à partir de l'origine (point) du brep à observer, on rate une intersection 
-                              # (x max) dans le cas ou on aurait à observer un quadrilatère sans angle droit ni parallèles (ou juste si l'un des cotés du quadrilatère 
-                              # n'est pas parallèle au vecteur x de l'origine)
-                              # RESOLUTION : TOURNER LA FORME ? 
-
-# Verifier si le vecteur résultant de 'corner3 = [max(x), max(y), 0]' et corner2 = [max(x), min(y), 0]' a deux intersections (avec le brep à observer);
-# Si = true, alors on passe à l'étape d'après
-# Si = false, alors on doit prolonger le vecteur jusqu'à une intersection avec le brep (en corner2 [max(x), min(y), 0]) et créer une nouvelle surface à remplir
-
-# Comparer la surface obtenue qui fait [corners1, corner2, corner3, corner 4] avec la database pour vérifier si y'a un recoupement avec le max(y) et la planche qu'on veut utiliser
-
-# Check si on peut la couper et créer une chute selon les parmaètres en minWasteLength;
-# Si = true, alors on la coupe et stocke provisoirement la chute créée 'C'
-# Si = false, on essaye avec la prochaine planche de la liste jusqu'à ce que ce soit =true
-
-# On répète l'opération pour remplir le brep
-
-           # PB: que se passe-t-il quand on arrive au bout du brep et qu'il reste un angle contenu dans une poutre ? 
-           # 
+corner4 = [min(x), max(y), 0] 
            
 breps = rs.AddPolyline([corner1, corner2, corner3, corner4, corner1])
 
